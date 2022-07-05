744bd05e61fd30916f8207ff0f6cd8145b8c6c67
ottocsb <83228451+ottocsb@users.noreply.github.com>
2022-07-05 20:02:59 +0800

    Update qianjivip.js

--- src/modules/qianjivip.js
+++ src/modules/qianjivip.js
@@ -1,10 +1,17 @@
 
 /*
-解锁钱迹测试
-*/
-let body = $response.body.replace(/"vipstart" : -1/g,'"vipstart":0')
-body.$response.body.replace(/"vipend" : -1/g,'"vipend":0')
-body.$response.body.replace(/"viptype" : -1/g,'"viptype":100')
+钱迹
+***************************
+QuantumultX:
+[rewrite_local]
+^https?:\/\/qianji\.xxoojoke\.com\/vip\/configios script-response-body https://raw.githubusercontent.com/ottocsb/tov-template/main/src/modules/qianjivip.js
+[mitm]
+hostname = qianji.xxoojoke.com
+**************************/
+let body = $response.body
+	.replace(/"vipstart" : -1/g,'"vipstart":0')
+	.replace(/"vipend" : -1/g,'"vipend":0')
+	.replace(/"viptype" : -1/g,'"viptype":100')
 $.log("进入钱迹")
 $.log("进入钱迹")
 $done({ body });
