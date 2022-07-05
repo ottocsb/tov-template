c6424d1d4558bf378f7410e579585a2b2cb547ad
ottocsb <83228451+ottocsb@users.noreply.github.com>
2022-07-05 19:40:54 +0800

    Update qianjivip.js

--- src/modules/qianjivip.js
+++ src/modules/qianjivip.js
@@ -6,4 +6,5 @@ let body = $response.body.replace(/"vipstart" : -1/g,'"vipstart":0')
 body.$response.body.replace(/"vipend" : -1/g,'"vipend":0')
 body.$response.body.replace(/"viptype" : -1/g,'"viptype":100')
 $.log("进入钱迹")
+$.log("进入钱迹")
 $done({ body });
