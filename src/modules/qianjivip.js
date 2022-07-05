93f3c5565864291f1a1760e6fbec553eac3dd230
ottocsb <83228451+ottocsb@users.noreply.github.com>
2022-07-05 20:50:11 +0800

    Update qianjivip.js

--- src/modules/qianjivip.js
+++ src/modules/qianjivip.js
@@ -10,9 +10,8 @@ hostname = qianji.xxoojoke.com
 **************************/
 
 var body = $response.body
-	.replace(/"vipstart" : -1/g,'"vipstart":0')
-	.replace(/"vipend" : -1/g,'"vipend":0')
-	.replace(/"viptype" : -1/g,'"viptype":100')
-console.log（body）
-console.log（"我是你爹"）
+	.replace(/"vipstart" : -1/g, "\"vipstart\":0")
+	.replace(/"vipend" : -1/g, "\"vipend\":0")
+	.replace(/"viptype" : -1/g, "\"viptype\":100");
 $done({ body });
+
