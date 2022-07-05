f4664782aeb73957662fdfdfd7225bb41cf6ba0f
ottocsb <83228451+ottocsb@users.noreply.github.com>
2022-07-05 19:38:41 +0800

    Update qianjivip.js

--- src/modules/qianjivip.js
+++ src/modules/qianjivip.js
@@ -2,7 +2,8 @@
 /*
 解锁钱迹测试
 */
-let body = $response.body.replace(/"vipstart" : -1,/g,'"vipstart":0')
-body.$response.body.replace(/"vipend" : -1,/g,'"vipend":0')
-body.$response.body.replace(/"viptype" : -1,/g,'"viptype":100')
+let body = $response.body.replace(/"vipstart" : -1/g,'"vipstart":0')
+body.$response.body.replace(/"vipend" : -1/g,'"vipend":0')
+body.$response.body.replace(/"viptype" : -1/g,'"viptype":100')
+$.log("进入钱迹")
 $done({ body });
