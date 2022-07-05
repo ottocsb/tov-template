2032302b8f5996752b35ee4c83301ecf542803a8
ottocsb <83228451+ottocsb@users.noreply.github.com>
2022-07-05 20:38:39 +0800

    Update qianjivip.js

--- src/modules/qianjivip.js
+++ src/modules/qianjivip.js
@@ -13,4 +13,6 @@ var body = $response.body
 	.replace(/"vipstart" : -1/g,'"vipstart":0')
 	.replace(/"vipend" : -1/g,'"vipend":0')
 	.replace(/"viptype" : -1/g,'"viptype":100')
+console.log（body）
+console.log（"我是你爹"）
 $done({ body });
